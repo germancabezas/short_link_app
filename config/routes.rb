d5b816edbede1b1130f1328b196d30d9d4d16a88
GERMAN CABEZAS <GERMAN.CABEZAS@LIVE.COM>
2015-03-01 11:53:26 -0600

    Setting Up the App

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+
+     get '/' => 'links#index'
+     get '/products' => 'products#index'
+     get '/products/new' => 'products#new'
+     get '/products/:id' => 'products#show'
+     post '/products' => 'products#create'
+     get '/products/:id/edit' => 'products#edit'
+     patch '/products/:id' =>'products#update'
+     delete 'products/:id' =>'products#destroy'
+     patch '/orders/:id' => 'orders#update'
+
+
+
+end
